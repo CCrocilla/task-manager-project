aabe429b2920af1bc341e93425572439e21a5bed
CCrocilla <cla.crocilla@gmail.com>
2022-06-05 23:12:24 +0000

    Add Update and Delete Functionality to Tasks

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -24,6 +24,5 @@ class Task(db.Model):
     def __repr__(self):
         # __repr__ to rappresent itself in the form of a string
         return "#{0} - Task: {1} | Urgent: {2}".format(
-            self.id, self.task_name, self.is_urgent        
-            
+            self.id, self.task_name, self.is_urgent   
         )
